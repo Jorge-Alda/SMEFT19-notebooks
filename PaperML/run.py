2fc755155fa3ec1bd34d28369d71567d3ce578f7
Jorge-Alda <jalda@unizar.es>
2023-11-13 15:18:21 +0100

    Likelihood plots for scenario BIII

--- PaperML/run.py
+++ PaperML/run.py
@@ -1,22 +1,25 @@
+#!/usr/bin/env python3
+
 import warnings
 from SMEFT19 import likelihood_fits
-from SMEFT19.scenarios import rotBII
+from SMEFT19.scenarios import rotBIII
 import SMEFT19
 from copy import deepcopy
 from pathlib import Path
 
 
 my_path = Path(__file__).parent
-d_ell = SMEFT19.ellipse.load(my_path.parent / "data" / "ellipses" / 'rotBII.yaml')
+d_ell = SMEFT19.ellipse.load(my_path.parent / "data" / "ellipses" / 'rotBIII.yaml')
 bf = d_ell['bf']
 
-dim_min = [-0.3, -0.08, -0.025, -0.15, 0]
-dim_max = [0, 0.08, 0.025, 0.15, 3.0]
-N = 5
 
-coefs = ['C', 'al', 'bl', 'aq', 'bq']
+dim_min = [-0.3, -0.3, 0]
+dim_max = [0, 0.0, 3.0]
+N = 50
 
-for (fit_x, fit_y) in [('C', 'al'), ('C', 'bl'), ('C', 'aq'), ('C', 'bq'), ('al', 'aq'), ('al', 'bq'), ('bl', 'aq')]:
+coefs = ['C1', 'C3', 'bq']
+
+for (fit_x, fit_y) in [('C1', 'C3'), ('C1', 'bq'), ('C3', 'bq')]:
 
     id_x = coefs.index(fit_x)
     id_y = coefs.index(fit_y)
@@ -36,20 +39,21 @@ for (fit_x, fit_y) in [('C', 'al'), ('C', 'bl'), ('C', 'aq'), ('C', 'bq'), ('al'
             lh_point = deepcopy(bf)
             lh_point[id_x] = x
             lh_point[id_y] = y
-            lg = likelihood_fits(lh_point, rotBII)
+            lg = likelihood_fits(lh_point, rotBIII)
             # print(f"{x:.4f}\t{y:.4f}\t{lg['global']:.4f}")
             return {k: max(v, -100) for k, v in lg.items()}
     try:
-        with open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBII_{fit_x}{fit_y}.dat', 'rt') as f_global:
+        with open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBIII_{fit_x}{fit_y}.dat', 'rt') as f_global:
             t = f_global.read()
             calculated = t.count('\t') + t.count('\n')
     except:
         calculated = 0
-
-    with open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBII_{fit_x}{fit_y}.dat', 'at') as f_global, \
-            open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBII_{fit_x}{fit_y}_RK.dat', 'at') as f_RK, \
-            open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBII_{fit_x}{fit_y}_RD.dat', 'at') as f_RD, \
-            open(my_path.parent / "data" / "likelihood" /  f'likelihood_rotBII_{fit_x}{fit_y}_LFV.dat', 'at') as f_LFV:
+     
+    with open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBIII_{fit_x}{fit_y}.dat', 'at') as f_global, \
+            open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBIII_{fit_x}{fit_y}_RK.dat', 'at') as f_RK, \
+            open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBIII_{fit_x}{fit_y}_RD.dat', 'at') as f_RD, \
+            open(my_path.parent / "data" / "likelihood" /  f'likelihood_rotBIII_{fit_x}{fit_y}_LFV.dat', 'at') as f_LFV, \
+            open(my_path.parent / "data" / "likelihood" / f'likelihood_rotBIII_{fit_x}{fit_y}_bqnunu.dat', 'at') as f_bqnunu:
         for i in range(calculated, N**2):
             lg = lh(i)
             if i % N == N-1:
@@ -58,6 +62,7 @@ for (fit_x, fit_y) in [('C', 'al'), ('C', 'bl'), ('C', 'aq'), ('C', 'bq'), ('al'
                 f_RK.flush()
                 f_RD.flush()
                 f_LFV.flush()
+                f_bqnunu.flush()
                 print(f"Plot for {fit_x}-{fit_y}: {i}/{N**2} ({i/N**2*100:.2f}%)")
             else:
                 sep = '\t'
@@ -65,4 +70,4 @@ for (fit_x, fit_y) in [('C', 'al'), ('C', 'bl'), ('C', 'aq'), ('C', 'bq'), ('al'
             f_RK.write(f'{lg["likelihood_lfu_fcnc.yaml"]}{sep}')
             f_RD.write(f'{lg["likelihood_rd_rds.yaml"]}{sep}')
             f_LFV.write(f'{lg["likelihood_lfv.yaml"]}{sep}')
-            # print(i)
+            f_bqnunu.write(f'{lg["likelihood_bqnunu.yaml"]}{sep}')
